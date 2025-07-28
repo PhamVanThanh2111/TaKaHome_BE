984fa0cf0d279a55487d8eacc0ec1fa65c695c88
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-07-28 09:45:33 +0700

    authorization for account

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -5,20 +5,32 @@ import {
   Patch,
   Param,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { AccountService } from './account.service';
 import { UpdateAccountDto } from './dto/update-account.dto';
 import { AccountResponseDto } from './dto/account-response.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from '../core/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../core/auth/guards/roles.guard';
 
 @ApiTags('accounts')
 @Controller('accounts')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
   @Get()
   @ApiOperation({ summary: 'Lấy danh sách tài khoản' })
   @ApiResponse({ status: HttpStatus.OK, type: [AccountResponseDto] })
+  @Roles('ADMIN')
   async findAll(): Promise<AccountResponseDto[]> {
     const accounts = await this.accountService.findAll();
     return accounts.map((acc) => ({
